87e274b83546febedae631a3259263b8e58546f6
VictorLCosta <victorlc2019@outlook.com>
2020-04-03 20:12:51 -0300

    Entities Contract and Installments implemented

---
+++ Entities/Contract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlinePaymentSystem.Entities
+{
+    class Contract
+    {
+        public int Number { get; private set; }
+        public  DateTime Date { get; private set; }
+        public double TotalValue { get; private set; }
+        public List<Installment> Installments { get; set; }
+
+        public Contract()
+        {
+        }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+    }
+}
---
+++ Entities/Installment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace OnlinePaymentSystem.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; private set; }
+        public double Amount { get; private set; }
+
+        public Installment()
+        {
+        }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{DueDate.ToShortDateString()} - {Amount.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
